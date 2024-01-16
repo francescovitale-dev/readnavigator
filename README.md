3d6d8d1d20e01290ac655bd208a4c0a4011ffe45
Francesco  Vitale <francescovitale@MacBook-Air-di-Francesco.local>
2024-01-16 23:59:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# ReadNavigator
+
+## Explore Books with ReadNavigator
+
+[![Sweetalert2](https://img.shields.io/badge/Sweetalert2-v11.0.20-orange)](https://cdn.jsdelivr.net/npm/sweetalert2@11.0.20/dist/sweetalert2.min.js)
+[![Bootstrap](https://img.shields.io/badge/Bootstrap-v5.3.2-blue)](https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js)
+
+- [Overview](#overview)
+- [Features](#features)
+- [How To Use](#how-to-use)
+- [Credits](#credits)
+
+## Overview
+
+ReadNavigator is a web application that allows you to explore books based on genres using the Open Library API. It provides a user-friendly interface to search for books, view details, and navigate through paginated results.
+
+![screenshot](src/assets/images/screenshot.png)
+
+**Live Demo**: [ReadNavigator](https://readnavigator.netlify.app/)
+
+## Features
+
+- Search for books by genre
+- Responsive design for seamless use on various devices
+- Integration with Sweetalert2 for enhanced user experience
+- Utilizes Bootstrap for a modern and clean UI
+
+## How To Use
+
+To use ReadNavigator, follow these steps:
+
+1. Clone this repository:
+
+    ```bash
+    git clone https://github.com/francescovitale-dev/readnavigator.git
+    ```
+
+2. Open the `index.html` file in your web browser.
+
+3. Enter a genre in the search bar and click the search button to explore books.
+
+## Credits
+
+ReadNavigator utilizes the following technologies, languages, and libraries:
+
+- [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+- [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+- [Bootstrap](https://getbootstrap.com/)
+- [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+- [Sweetalert2](https://sweetalert2.github.io/)
+
+---
+
+> [vitalefrancesco.com](https://www.vitalefrancesco.com) &nbsp;&middot;&nbsp;
+> GitHub [@francescovitale-dev](https://github.com/francescovitale-dev)
---
+++ src/assets/js/main.js
@@ -0,0 +1,125 @@
+import axios from "axios";
+import _ from "lodash";
+import Swal from "sweetalert2";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../style/style.css";
+import "../style/footer.css";
+
+export default function mainContent() {
+  const itemsPerPage = 9;
+
+  // Function to handle the search by genre
+  async function searchByGenre(event) {
+    // Prevent the default form submission
+    event.preventDefault();
+    const genre = _.toLower(document.getElementById("genreInput").value);
+
+    // Display loading message
+    const loadingMessage = document.getElementById("loadingMessage");
+    loadingMessage.style.display = "block";
+
+    try {
+      // Use Axios to fetch data from the Open Library API based on the genre
+      const response = await axios.get(
+        `https://openlibrary.org/subjects/${genre}.json?details=true&limit=${itemsPerPage}`
+      );
+
+      if (!response.status === 200) {
+        throw new Error("Error");
+      }
+
+      const data = response.data;
+      const subjectResult = document.getElementById("js-subject-result");
+
+      // Handle case when no works are found for the given genre
+      if (!data.works || data.works.length === 0) {
+        Swal.fire({
+          icon: "info",
+          title: "Info",
+          text: `Please enter a valid genre.`,
+        });
+        // Clear the result and hide loading
+        loadingMessage.style.display = "none";
+        subjectResult.innerHTML = "";
+      } else {
+        // Hide loading
+        loadingMessage.style.display = "none";
+
+        // Generate HTML for displaying search results
+        const htmlString = _.map(
+          data.works,
+          (work, index) => `
+            <div class="result-item card mb-3 col-sm-12 col-md-6 col-lg-4 col-xl-4 mx-auto" style="border: solid 10px #d2b48c; width: 250px; position: relative;">
+                <div class="card-body">
+                    <h3 class="card-title text-dark">${work.title}</h3>
+                    <p class="card-text text-dark author"><strong>Author:</strong> ${
+                      work.authors[0]?.name
+                    }</p>
+                    <button type="button" class="btn js-show-description description-button" data-key="${
+                      work.key
+                    }" style="position: absolute; bottom: 10px; width: 180px; text-align: center; border: solid 1px #d2b48c;">Show Description</button>
+                </div>
+            </div>
+            ${index % 3 === 2 ? '</div><div class="row">' : ""}
+          `
+        ).join(" ");        
+
+        // Display the search results
+        subjectResult.innerHTML = `<div class="row">${htmlString}`;
+
+        const descriptionButtons = document.querySelectorAll(
+          ".js-show-description"
+        );
+        descriptionButtons.forEach((button) => {
+          button.addEventListener("click", () => {
+            const key = button.getAttribute("data-key");
+            const title = button.getAttribute("data-title");
+            getBookDescription(key, title);
+          });
+        });
+      }
+    } catch (error) {
+      loadingMessage.style.display = "none";
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please enter a valid genre.",
+      });
+
+      const subjectResult = document.getElementById("js-subject-result");
+      subjectResult.innerHTML = "";
+      return;
+    }
+  }
+
+  // Function to fetch and display book description
+  async function getBookDescription(key, title) {
+    const descriptionUrl = `https://openlibrary.org${key}.json`;
+
+    try {
+      const response = await axios.get(descriptionUrl);
+      const data = response.data;
+
+      if (typeof data.description === "string") {
+        Swal.fire({
+          title: `${data.title}`,
+          text: `${data.description}`,
+        });
+      } else {
+        Swal.fire({
+          icon: "info",
+          title: `${data.title}`,
+          text: "Oops! Sorry, there is no description available for this book.",
+        });
+      }
+    } catch (error) {
+      console.error("Error retrieving book description:", error);
+    }
+  }
+
+  // Get the search button and add an event listener to trigger the search by genre
+  const searchButton = document.querySelector(".js-btn");
+  searchButton.addEventListener("click", searchByGenre);
+}
+
+mainContent();
